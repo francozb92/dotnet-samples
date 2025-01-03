3a7584f182372a6eaeb2c18d641fdc1180a605d4
Franco Zavagnini <francozb92@gmail.com>
2025-01-03 16:00:49 -0400

    Started a feature switch API example to represent a different way to version an API

--- 9.0/runtime/feature-switches/Program.cs
+++ 9.0/runtime/feature-switches/Program.cs
@@ -1,17 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics.CodeAnalysis;
 using BenchmarkDotNet.Running;
-
-Console.WriteLine("This is a sample project about feature switches in dotnet 9.0 runtime");
-var summary = BenchmarkRunner.Run<FeatureSwitchesBenchmarks>();
+using Microsoft.EntityFrameworkCore;
 
 
-//Base example
+// Console.WriteLine("This is a sample project about feature switches in dotnet 9.0 runtime");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+if(APIFeature.OnlyTodo){
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+}
+
+if(APIFeature.OnlyCompleted){
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+}
+
+app.Run();
+
+
+// //Base example
 public class Feature
 {
     [FeatureSwitchDefinition("Feature.IsSupported")]
     internal static bool IsSupported => AppContext.TryGetSwitch("Feature.IsSupported", out bool isEnabled) ? isEnabled : true;
-    internal static bool IsTrimmed => AppContext.TryGetSwitch("Feature.IsTrimmed", out bool isTrimmed) ? isTrimmed : true;
     internal static void Implementation() => Console.WriteLine("Hello from feature enabled!");
 }
+
+//API example using feature switches 
+public class APIFeature
+{
+    [FeatureSwitchDefinition("APIFeature.OnlyCompleted")]
+    internal static bool OnlyCompleted => AppContext.TryGetSwitch("APIFeature.OnlyCompleted", out bool onlyCompleted) ? onlyCompleted : true;
+    [FeatureSwitchDefinition("APIFeature.OnlyTodo")]
+    internal static bool OnlyTodo => AppContext.TryGetSwitch("APIFeature.OnlyTodo", out bool onlyTodo) ? onlyTodo : true;
+}
+
+
+
+
 
--- 9.0/runtime/feature-switches/README.md
+++ 9.0/runtime/feature-switches/README.md
@@ -4,6 +4,8 @@
 
 # feature switches
 
+- Base Example
+
 - Running and testing feature switches with benchmarking capabilities using BenchmarkDotNet
 
 * Results
@@ -24,3 +26,11 @@ Intel Core i7-2620M CPU 2.70GHz (Sandy Bridge), 1 CPU, 4 logical and 2 physical 
 | **FeatureSwitches_Benchmark** | **True**        | **746,077.2467 ns** | **198,860.3743 ns** | **537,630.6247 ns** | **589,153.7598 ns** |         **-** |
 
 - We can conclude there is a significant percentage difference between both benchmarks
+
+- API Example
+
+This example using feature switches in dotnet 9 effectively demonstrates a different way of versioning an API (Obsolete, Deprecated, Outdated, NotRelevant, etc.) when publishing to different consumers and their requirements where an exposed endpoint does not need to be available thus reducing application size and conformance.
+
+**Note that feature switches are only available with minimal APIs and not for controller based APIs**
+
+**Software teams benefit tremendously when adopting minimal APIs due to modern ways of architecting solutions (microservices architecture) and their ability to adapt to change in high paced agile environments**
